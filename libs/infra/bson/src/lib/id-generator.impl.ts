8237740e329529e39a7a81c07f58a2031884ceb6
ayoung4 <alfredsyoung@gmail.com>
2023-05-19 00:15:37 -0700

    simplify bson id generator

--- libs/infra/bson/src/lib/id-generator.impl.ts
+++ libs/infra/bson/src/lib/id-generator.impl.ts
@@ -1,14 +1,9 @@
 import * as L from '@effect-ts/core/Effect/Layer'
-import * as S from '@effect-ts/core/Sync'
 import * as T from '@effect-ts/core/Effect'
 import { ObjectId } from 'bson'
 
 import { IdGenerator } from '@eirene-onboarding-3/model'
 
-const makeLive = S.succeedWith(
-  (): IdGenerator => ({
-    generate: T.succeedWith(() => new ObjectId().toString()),
-  })
-)
-
-export const BSONIdGenerator = L.fromEffect(IdGenerator)(makeLive)
+export const BSONIdGenerator = L.fromValue(IdGenerator)({
+  generate: T.succeedWith(() => new ObjectId().toString()),
+})
